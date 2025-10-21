0009c09fee69bac4705dc26c693ad77ef2d754c7
AlfredoLG <dluxg1@miumg.edu.gt>
2025-10-20 18:31:30 -0600

    :wrench: se agregaron archivos de estructura, para mejorar mantenimimiento y mejoras futuras

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.umg.booksphere {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.umg.booksphere to javafx.fxml;
     exports com.umg.booksphere;
