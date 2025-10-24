d01918dda37370b6562eb6b982aa00f855423399
AlfredoLG <dluxg1@miumg.edu.gt>
2025-10-24 11:22:23 -0600

    :wrench: se Configuro la conexion a la base de datos

--- src/main/java/com/umg/booksphere/model/ConexionDB.java
+++ src/main/java/com/umg/booksphere/model/ConexionDB.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.umg.booksphere.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author Alfredo
  */
 public class ConexionDB {
+    // variables de entorno 
+    private static final String URL= "";
+    private static final String USER ="";
+    private static final String PASS ="";
     
+    
+    private static Connection con;
+    
+    public static Connection getConexion(){
+    try{
+        if(con == null || con.isClosed()){
+        con = DriverManager.getConnection(URL,USER,PASS);
+        System.out.println("Conexión exitosa a la base de datos.");
+        }
+    }catch (SQLException e) {
+            System.err.println("Error al conectar: " + e.getMessage());
+        }
+        return con;   
+    }
+   
+    public static void closeConnection() {
+        try {
+            if (con != null && !con.isClosed()) {
+                con.close();
+                System.out.println("🔌 Conexión cerrada correctamente.");
+            }
+        } catch (SQLException e) {
+            System.err.println("⚠️ Error al cerrar la conexión: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.umg.booksphere {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
 
     opens com.umg.booksphere to javafx.fxml;
     exports com.umg.booksphere;
